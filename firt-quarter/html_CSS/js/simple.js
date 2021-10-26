e73fc55cda2db89cfd69552ebcbfe21f84c54d7a
artem1426 <bsed00@mail.ru>
2021-10-26 23:14:36 +0300

    finally
    finally

--- firt-quarter/html_CSS/js/simple.js
+++ firt-quarter/html_CSS/js/simple.js
@@ -1,30 +1,5 @@
-$(document).ready(function(){
-    $('button.menu-left-button').on('click', function (event){//TODO выпадающий список: main, contact
-        if (event && event.detail === 1)
-            location.href='./contact.html';
-    });
-    $('button.menu-rigth-button').on('click', function (event){
-        if (event && event.detail === 1)
-            location.href='./main.html';//TODO выпадающий список: лк, настройки, авторизоваться/выйти
-    });
-    $('button.see-other-project').on('click', function (event){
-        if (event && event.detail === 1)
-            location.href='./contact.html';//TODO переход на строничку с проектами
-    });
-    $('#learn_more_1').on('click', function (event){
-        if (event && event.detail === 1)
-            location.href='./contact.html';//TODO 
-    });
-    $('#learn_more_2').on('click', function (event){
-        if (event && event.detail === 1)
-            location.href='./contact.html';//TODO 
-    });
-    $('#learn_more_3').on('click', function (event){
-        if (event && event.detail === 1)
-            location.href='./contact.html';//TODO 
-    });
-    $('button.button-footer').on('click', function (event){
-        if (event && event.detail === 1)
-            location.href='./contact.html';
-    });
+$(document).ready(function () {
+    $('title').text('ITechnologi');//Меняем название
+    $('<link rel="icon" type="image/png" href="./img/favicon-main.png">').appendTo('head');//устанв. иконку сайта
+    
 });
