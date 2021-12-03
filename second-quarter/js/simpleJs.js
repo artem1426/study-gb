b1edc8e273abcb37b06d3683c7b5c9372746daac
artem1426 <bsed00@mail.ru>
2021-12-03 19:54:41 +0300

    lesson 5

--- second-quarter/js/simpleJs.js
+++ second-quarter/js/simpleJs.js
@@ -1,5 +1,8 @@
 //Добавлено только для сахара и простейшего функционала
 /* Пути */
+const goMain = () => {
+    document.location.href = '../pages/index.html';
+}
 const goProduct = (path) => {
     localStorage.setItem('pathImg', path);
     document.location.href = '../pages/product.html';
@@ -7,20 +10,22 @@ const goProduct = (path) => {
 const goBasket = () => {
     document.location.href = '../pages/cart.html';
 }
-
+const goCheckOut = () => {
+    document.location.href = '../pages/registration.html';
+}
 /* Взаимодействия с куками и памятью */
-if (document.location.href.indexOf('product.html') !== -1 && localStorage.getItem('pathImg')){
+if (document.location.href.indexOf('product.html') !== -1 && localStorage.getItem('pathImg')) {
     const carouselMain = document.getElementById('carousel_default')
     carouselMain.setAttribute('src', localStorage.getItem('pathImg'))
-}else if (document.location.href.indexOf('product.html') !== -1) {
+} else if (document.location.href.indexOf('product.html') !== -1) {
     localStorage.removeItem('pathImg');
     alert('перешли на страницу по ссылке?' +
         '\nМожешь посмотреть, но через 10 сек будешь отправлен обратно на страницу каталога.' +
         '\nЧтобы зайти на эту страницу, выбери товар на сайте и нажмите на него.');
-    setInterval(()=>{
+    setInterval(() => {
         document.location.href = '../pages/catalog.html';
     }, 10000);
-}else localStorage.removeItem('pathImg');
+} else localStorage.removeItem('pathImg');
 const addToCart = () => {
     const cartNum = parseInt(localStorage.getItem('cart_num'));
     localStorage.setItem('cart_num', isNaN(cartNum) ? 1 : cartNum + 1);
@@ -31,7 +36,9 @@ if (localStorage.getItem('cart_num'))
 
 /* Слайдер*/
 let slideIndex = 1;
-showSlides(slideIndex);
+if (document.location.href.indexOf('product.html') !== -1) {
+    showSlides(slideIndex);
+}
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
@@ -45,14 +52,24 @@ function showSlides(n) {
     let i;
     const slides = document.getElementsByClassName("mySlider");
     const dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+const clearLocalStorage = () => {
+    localStorage.clear();
+    localStorage.setItem('cart_num', '0');
+    document.getElementById('cart_counter').textContent = localStorage.getItem('cart_num');
 }
